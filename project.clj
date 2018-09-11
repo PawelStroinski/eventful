e1d592048dbf96cae9e6d47b48f81863ab319c13
Paweł Stroiński <pol84@live.com>
2018-09-11 23:10:49 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/PawelStroinski/eventful"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.geteventstore/eventstore-client_2.12 "5.0.1"]
-                 [manifold "0.1.6"]]
+  :dependencies [[com.geteventstore/eventstore-client_2.12 "5.0.8"]
+                 [manifold "0.1.8"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox
   {:namespaces [eventful.core]
    :source-uri
    "https://github.com/PawelStroinski/eventful/blob/{version}/{filepath}#L{line}"}
-  :profiles {:dev {:dependencies [[midje "1.9.1"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [midje "1.9.1"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [cheshire "5.7.0"]]}})
+                                  [cheshire "5.8.0"]]}})
