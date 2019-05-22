dc1eaf415f4da9b60bf288a1534bd345b0382cc1
Paweł Stroiński <pol84@live.com>
2019-05-23 00:25:44 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/PawelStroinski/eventful"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.geteventstore/eventstore-client_2.12 "5.0.8"]
-                 [manifold "0.1.8"]
+  :dependencies [[com.geteventstore/eventstore-client_2.12 "5.0.9"]
+                 [manifold "0.1.6"]
                  [com.cognitect/transit-clj "0.8.309"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.7"]]
   :codox
   {:namespaces [eventful.core]
    :source-uri
